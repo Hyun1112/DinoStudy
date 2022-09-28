0c5cff2bd90d246d08c1bdd910eb02f398e4a4b5
Hyun1112 <68471537@naver.com>
2022-09-28 16:44:01 +0900

    First Commit

---
+++ app/src/main/java/com/example/dinostudy/Fragment_chart.java
@@ -0,0 +1,23 @@
+package com.example.dinostudy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment_chart extends Fragment {
+
+    public Fragment_chart(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chart, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/dinostudy/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.dinostudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton btn_todo, btn_chart, btn_watch, btn_commu, btn_diary, btn_heart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_todo = (ImageButton)findViewById(R.id.btn_chk);
+        btn_chart = (ImageButton)findViewById(R.id.btn_chart);
+        btn_watch = (ImageButton)findViewById(R.id.btn_watch);
+        btn_commu = (ImageButton)findViewById(R.id.btn_commu);
+        btn_diary = (ImageButton)findViewById(R.id.btn_diary);
+        btn_heart = (ImageButton)findViewById(R.id.btn_heart);
+
+        btn_todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_todo fragment_todo = new Fragment_todo();
+                transaction.replace(R.id.frame,fragment_todo);
+                transaction.commit();
+
+                btn_todo.setImageResource(R.drawable.checkbox_y);
+                btn_chart.setImageResource(R.drawable.chart_n);
+                btn_watch.setImageResource(R.drawable.watch_n);
+                btn_commu.setImageResource(R.drawable.bubble_n);
+                btn_diary.setImageResource(R.drawable.diary_n);
+                btn_heart.setImageResource(R.drawable.heart_n);
+            }
+        });
+
+        btn_chart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_chart fragment_chart = new Fragment_chart();
+                transaction.replace(R.id.frame,fragment_chart);
+                transaction.commit();
+
+                btn_todo.setImageResource(R.drawable.checkbox_n);
+                btn_chart.setImageResource(R.drawable.chart_y);
+                btn_watch.setImageResource(R.drawable.watch_n);
+                btn_commu.setImageResource(R.drawable.bubble_n);
+                btn_diary.setImageResource(R.drawable.diary_n);
+                btn_heart.setImageResource(R.drawable.heart_n);
+            }
+        });
+
+        btn_watch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_watch fragment_watch = new Fragment_watch();
+                transaction.replace(R.id.frame,fragment_watch);
+                transaction.commit();
+
+                btn_todo.setImageResource(R.drawable.checkbox_n);
+                btn_chart.setImageResource(R.drawable.chart_n);
+                btn_watch.setImageResource(R.drawable.watch_y);
+                btn_commu.setImageResource(R.drawable.bubble_n);
+                btn_diary.setImageResource(R.drawable.diary_n);
+                btn_heart.setImageResource(R.drawable.heart_n);
+            }
+        });
+
+        btn_commu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_community fragment_community = new Fragment_community();
+                transaction.replace(R.id.frame,fragment_community);
+                transaction.commit();
+
+                btn_todo.setImageResource(R.drawable.checkbox_n);
+                btn_chart.setImageResource(R.drawable.chart_n);
+                btn_watch.setImageResource(R.drawable.watch_n);
+                btn_commu.setImageResource(R.drawable.bubble_y);
+                btn_diary.setImageResource(R.drawable.diary_n);
+                btn_heart.setImageResource(R.drawable.heart_n);
+            }
+        });
+
+        btn_diary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_diary fragment_diary = new Fragment_diary();
+                transaction.replace(R.id.frame,fragment_diary);
+                transaction.commit();
+
+                btn_todo.setImageResource(R.drawable.checkbox_n);
+                btn_chart.setImageResource(R.drawable.chart_n);
+                btn_watch.setImageResource(R.drawable.watch_n);
+                btn_commu.setImageResource(R.drawable.bubble_n);
+                btn_diary.setImageResource(R.drawable.diary_y);
+                btn_heart.setImageResource(R.drawable.heart_n);
+            }
+        });
+
+        btn_heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_heart fragment_heart = new Fragment_heart();
+                transaction.replace(R.id.frame,fragment_heart);
+                transaction.commit();
+
+                btn_todo.setImageResource(R.drawable.checkbox_n);
+                btn_chart.setImageResource(R.drawable.chart_n);
+                btn_watch.setImageResource(R.drawable.watch_n);
+                btn_commu.setImageResource(R.drawable.bubble_n);
+                btn_diary.setImageResource(R.drawable.diary_n);
+                btn_heart.setImageResource(R.drawable.heart_y);
+            }
+        });
+    }
+}
