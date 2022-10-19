8b12821da9da663098703f5a18321a4a0650f62c
Hyun1112 <68471537@naver.com>
2022-10-19 15:25:26 +0900

    차트 구현 중(10%)

--- app/src/main/java/com/example/dinostudy/Fragment_chart.java
+++ app/src/main/java/com/example/dinostudy/Fragment_chart.java
@@ -1,5 +1,7 @@
 package com.example.dinostudy;
 
+import android.app.TabActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,16 +10,30 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class Fragment_chart extends Fragment {
-
     public Fragment_chart(){
 
     }
 
+    ViewPager viewPager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chart, container, false);
+        View view = inflater.inflate(R.layout.fragment_chart, container, false);
+
+        viewPager = view.findViewById(R.id.viewpager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
     }
 }
